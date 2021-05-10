f5005e19768d3d2a780267094f3629c6fa1e14ab
zaxirolls <zaxirolls007@gmail.com>
2021-05-10 22:31:28 +0900

    update

---
+++ 独習python/for_list.py
@@ -0,0 +1,3 @@
+data = [15,43,7,59,98]
+data2 = [i * 2 for i in data]
+print(data2)
---
+++ 独習python/for_practice.py
@@ -0,0 +1,5 @@
+for i in range(1,10):
+    for j in range(1,10):
+        a = i * j
+        print(a,end=" ")
+    print()
