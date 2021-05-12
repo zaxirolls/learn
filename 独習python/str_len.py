6e900137fbaca23d6b5bb329ad35d6f533db020d
zaxirolls <zaxirolls007@gmail.com>
2021-05-12 22:58:26 +0900

    str_len,lower

---
+++ 独習python/str_len.py
@@ -0,0 +1,2 @@
+title = 'WiNGSプロジェクト'
+print(len(title))
---
+++ 独習python/str_lower.py
@@ -0,0 +1,10 @@
+data1 = "Wings Project"
+data2 = "self learn python"
+data3 = "Fuβball"
+
+print(data1.lower())
+print(data1.upper())
+print(data1.swapcase())
+print(data1.capitalize())
+print(data2.title())
+print(data3.casefold())
